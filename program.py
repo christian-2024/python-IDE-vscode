050fea813c237178f6bffe13b6d6537a2bc8fbf5
Christian Oliveira <145071784+christian-2024@users.noreply.github.com>
2025-10-03 20:00:08 -0300

    correção na lógica

--- program.py
+++ program.py
@@ -5,9 +5,16 @@ def calcular_imposto(salario):
     aliquota = 0.00
     if (salario >= 0 and salario <= 1100):
         aliquota = 0.05
+    elif (salario >= 1100 and salario <= 2500):
+        aliquota = 0.10
+    else:
+        aliquota = 0.15
     return aliquota * salario
 
+print("Qual valor do salário:")
 valor_salario = float(input())
+
+print("Qual valor do Beneficio: ")
 valor_beneficios = float(input())
 
 valor_imposto = calcular_imposto(valor_salario)
